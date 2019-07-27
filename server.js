d0878cfb55de0eb2ba9ec0a3fb405a19617aca80
John Azzaro <jazzaro@temple.edu>
2019-07-27 10:21:53 -0700

    static file server setup, public folder added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
 
-                                                                                  /* First, load any modules you need for your project */
-const express = require('express');                                                  /* in this case, we want to load express and store as a constant named express*/
+                                                                                  /* First, import any modules you need for your project */
+const express = require('express');                                                  /* in this case, we want to import express and store as a constant named express*/
                                                                                 
                                                                                    /* Second, initialize the express application from the module you previously loaded above... */
 const app = express();                                                                  /* ... and store as a constant named "app" (which we will now use for all our routes and other commands*/
+
+
+app.use(express.static('public'));                                                /* set up your ststic file server to serve files from the "public" folder */
