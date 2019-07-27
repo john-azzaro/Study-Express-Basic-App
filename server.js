2b690191dd64c3b27669945494cd2d37320abf3b
John Azzaro <jazzaro@temple.edu>
2019-07-27 10:16:24 -0700

    express server

---
+++ server.js
@@ -0,0 +1,7 @@
+'use strict';
+
+                                                                                  /* First, load any modules you need for your project */
+const express = require('express');                                                  /* in this case, we want to load express and store as a constant named express*/
+                                                                                
+                                                                                   /* Second, initialize the express application from the module you previously loaded above... */
+const app = express();                                                                  /* ... and store as a constant named "app" (which we will now use for all our routes and other commands*/
