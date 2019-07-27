f20484ae84f552d53505cdbe0f80aef01cca5e0f
John Azzaro <jazzaro@temple.edu>
2019-07-27 11:26:01 -0700

    fixed, js issue, jquery added, logic, routes

---
+++ public/client.js
@@ -0,0 +1,27 @@
+
+
+function getCount() {
+    $.get('/the-count', function(data) {
+      $('.js-current-count').text(data.count);
+    });
+}
+
+function buttonPress() {
+    $('body').on('click', '.js-button', function() {
+        event.preventDefault();
+        getCount();
+    });
+}
+
+function setupEventListeners() {
+    buttonPress();
+}
+
+function initialize() {
+    setupEventListeners();
+}
+
+$(initialize);
+
+
+
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
                                                                                   /* First, import any modules you need for your project */
 const express = require('express');                                                  /* in this case, we want to import express and store as a constant named express*/
@@ -7,12 +7,24 @@ const express = require('express');                                             
 const app = express();                                                                  /* ... and store as a constant named "app" (which we will now use for all our routes and other commands*/
 
 
+let theCount = 0;
+
+
 app.use(express.static('public'));                                                /* set up your ststic file server to serve files from the "public" folder */
 
 
-app.get('/', function(req, res) {                                                 /* Testing route*/
-    console.log('This is a working test response... ')
-});
+app.get("/the-count", (req, res) => {
+    console.log('incrementing the count');
+    theCount += 1;
+    console.log(`the count is ${theCount}`);
+    res.json({count: theCount});
+  });
+
+
+
+// app.get('/', function(req, res) {                                                 /* Testing route*/
+//     console.log('This is a working test response... ')
+// });
 
 app.listen(3000, function() {                                                       /* listen on port 3000 for incoming requests*/
     console.log(`Your app is listening on port 3000...`);
